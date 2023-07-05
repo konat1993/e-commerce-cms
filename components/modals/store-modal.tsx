7bb26116b84562af9ad2dc63318317870da06b25
l-konatowski <konat1993@gmail.com>
2023-07-05 15:42:13 +0200

    created modal form and initialized prisma

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,9 +2,34 @@
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import { Modal } from "../ui/modal"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+
+const formSchema = z.object({
+    name: z.string().min(3, { message: "String must contain at least 3 character(s)" }),
+})
 
 const StoreModal = () => {
     const { isOpen, onClose } = useStoreModal()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: ""
+        }
+    })
+
+    const { handleSubmit, control } = form
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        // TODO: Create Store
+        console.log({values});
+        
+    }
 
     return (
         <Modal
@@ -13,7 +38,33 @@ const StoreModal = () => {
             isOpen={isOpen}
             onClose={onClose}
         >
-            StoreModal
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <FormField name="name" render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="E-Commerce" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                                control={control}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button variant="outline" onClick={onClose}>
+                                    Cancel
+                                </Button>
+                                <Button type="submit">
+                                    Create
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal>
     )
 }
